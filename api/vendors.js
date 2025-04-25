4cf0999a68ede3e31db20833e5863575dc53cc80
dioCleraer <su222kr@gmail.com>
2025-04-25 16:50:48 +0900

    chore: switch APIs to ESM

--- api/vendors.js
+++ api/vendors.js
@@ -1,6 +1,8 @@
-module.exports = (req, res) => {
+import sql from 'mssql';
+export default async function handler(req, res)
+ {
   res.status(200).json([
     { VENDOR_NO: "TEST01", VENDOR_NAME: "테스트업체1" },
     { VENDOR_NO: "TEST02", VENDOR_NAME: "테스트업체2" }
   ]);
-};v
+};
